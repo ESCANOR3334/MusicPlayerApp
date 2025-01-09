08cbd66c0ec67e63c2cfdd85907454eff9baf622
ESCANOR3334 <derdal2004@gmail.com>
2025-01-09 14:17:48 +0100

    modifier

--- script.js
+++ script.js
@@ -157,6 +157,26 @@ const pauseSong = () => {
   audio.pause();
 };
 
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  }else{
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
+  }
+}
+
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) {
+    return;
+  }else{
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+    playSong(previousSong.id);
+  }
+}
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
@@ -180,7 +200,9 @@ const renderSongs = (array) => {
 
 };
 
-
+const getCurrentSongIndex = () => {
+  return userData?.songs.indexOf(userData?.currentSong)
+}
 
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null){
@@ -192,6 +214,8 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+nextButton.addEventListener("click",playNextSong);
+previousButton.addEventListener("click",playPreviousSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
